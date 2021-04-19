4b116a88bae741359b0619d21d448acba1cac15f
chiefanunzio <chiefa.nunzio@gmail.com>
2021-04-19 19:01:43 +0200

    film e serie

--- js/main.js
+++ js/main.js
@@ -1,15 +1,86 @@
+function initVue() {
 
-function initVue(){
-
-    new initVue({
+    new Vue({
 
 
         el: '#app',
-        data:{
+        data: {
+
+            films: [],
+            series:[],
+
+        },
+        methods: {
+
+            getFilm: function () {
+
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+
+                        params: {
+
+                            'api_key': 'd9645c1e3498de1c3ee0657bb3a22a20',
+                            'query': this.searched,
+                            'language': 'it-IT',
 
 
-            
+
+                        }
+
+                    })
+                    .then(data => {
+
+                        this.films = data.data.results;
+
+                    })
+                    axios.get('https://api.themoviedb.org/3/search/tv', {
+
+                        params: {
+
+                            'api_key': 'd9645c1e3498de1c3ee0657bb3a22a20',
+                            'query': this.searched,
+                            'language': 'it-IT',
+
+
+
+                        }
+
+                    })
+
+                    .then(data => {
+
+                        this.series = data.data.results;
+
+                    })
+
+
+
+                         
+
+
+
+
+
+
+            }
         }
+        // computed: {
+
+        //     filteredFilms: function () {
+
+        //         for (let i = 0; i < this.films.length; i++) {
+        //             const elem = this.films[i];
+        //             const title = elem.title;
+        //             if (title.toLowercase().includes(this.searchFilm.toLowerCase())) {
+
+        //                 this.films.push(elem)
+        //             }
+
+
+        //         }
+        //         return this.films
+        //     }
+        // }
+
     })
 
 
@@ -22,9 +93,9 @@ function initVue(){
 
 
 
-function init(){
+function init() {
 
-    // initVue()
+    initVue()
 
 }
 
